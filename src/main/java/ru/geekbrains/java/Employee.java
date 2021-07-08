b20eb66d350042866c0374e8f20b7b060e5fefae
Владислав <kontinuum01@gmail.com>
2021-07-08 21:38:00 +0300

    OOP

--- src/main/java/ru/geekbrains/java/Employee.java
+++ src/main/java/ru/geekbrains/java/Employee.java
@@ -1,18 +1,20 @@
 package ru.geekbrains.java;
 
 public class Employee {
+    private static String fullname;
+    private static String position;
+    private static String email;
+    private static int numberphone;
+    private static int thesalary;
+    private static int age;
+
     public static void main(String[] args) {
 
         new Employee("Jackson Peter", "Manager", "jackson3302@mail.com", 5553302, 2500, 28);
-        persons();
-    }
+        print();
 
-    private String fullname;
-    private String position;
-    private String email;
-    private int numberphone;
-    private int thesalary;
-    private int age;
+
+    }
 
     public Employee(String fullname, String position, String email, int numberphone, int thesalary, int age) {
 
@@ -23,31 +25,16 @@ public class Employee {
         this.thesalary = thesalary;
         this.age = age;
 
-  //      System.out.println(" Employee Name: " + fullname + "; Position: " + position + "; Email: " + email +
-  //              "; Number Phone: " + numberphone + "; The Salary: " + thesalary + "$; Возраст: " + age);
     }
-
-    public int getAge() {
-    return this.age;}
-
-        public static void persons() {
-            Employee[] persArray = new Employee[5];
-            persArray[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", 892312312, 30000, 30);
-            persArray[1] = new Employee("Petrov Igor", "Master", "igor2008@mail.com", 892651254, 35000, 42);
-            persArray[2] = new Employee("Petrenko Artem", "Engineer", "petArt@mail.com", 892946768, 37500, 34);
-            persArray[3] = new Employee("Ekimov Anton", "Technolog", "antonmov@mail.com", 892978658, 33000, 50);
-            persArray[4] = new Employee("Protasov Jurii", "Engineer", "protjur1@mail.com", 892963054, 34500, 46);
-            for ( Employee employee : persArray){
-                if ( employee.getAge() > 40) employee.printInfo();
-
-            }
-    }
-    private void printInfo() {
+    private static void print(){
         System.out.println(" Employee Name: " + fullname + "; Position: " + position + "; Email: " + email +
-                "; Number Phone: " + numberphone + "; The Salary: " + thesalary + "$; Возраст: " + age);
+                "; Number Phone: " + numberphone + "; The Salary: " + thesalary + "$; Age: " + age);
     }
 
     }
+
+
+
 
 
 
