9824f4a7285727982bb4638958161b572eb4ada5
Владислав <kontinuum01@gmail.com>
2021-08-04 23:05:26 +0300

    Multithreading part.2

---
+++ src/main/java/race/Tunnel.java
@@ -0,0 +1,33 @@
+package race;
+
+import java.util.concurrent.Semaphore;
+import static race.Main.HALF_CARS_COUNT;
+
+public class Tunnel extends Stage{
+    //Добавляем объект синхронизации потоков  Semaphore
+    Semaphore smp = new Semaphore(HALF_CARS_COUNT);
+
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
